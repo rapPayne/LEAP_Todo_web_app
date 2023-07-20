99b91a18af20d290498f25c9b50cace66f98f09a
Chris Minnick <chris@minnick.com>
2023-07-20 14:08:12 -0400

    added code to generate a list item

--- client/main.js
+++ client/main.js
@@ -1,2 +1,10 @@
 let todoList = document.getElementById('todolist');
-todoList.innerHTML = 'You have no todo items';
+//todoList.innerHTML = 'You have no todo items';
+
+const newItem = document.createElement('li');
+newItem.classList.add('todo', 'red');
+newItem.classList.add('important');
+
+const newContent = document.createTextNode('do Something');
+newItem.appendChild(newContent);
+todoList.appendChild(newItem);
